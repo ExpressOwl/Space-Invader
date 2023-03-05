701e99889ec2bfdf76bfb6c1263fa808782fa36f
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-03-05 16:45:20 +0000

    Drew the aliens
    used for loop to draw the aliens in rows and columns and pushed  them onto an array which was declared as empty initially.

--- space.js
+++ space.js
@@ -50,27 +50,55 @@ window.onload = function () {
     context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
   };
 
+  alienImg = new Image();
+  alienImg.src = "./alien.png";
+  createAliens();
+
   requestAnimationFrame(update);
-  document.addEventListener('keydown', moveShip);
+  document.addEventListener("keydown", moveShip);
 };
 
 function update() {
-  requestAnimationFrame(update)
+  requestAnimationFrame(update);
 
-  context.clearRect(0, 0, board.width, board.height)
+  context.clearRect(0, 0, board.width, board.height);
 
   // ship redraw when moving
   context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+  // Alien draw
+  for (let i = 0; i < alienArray.length; i++) {
+    let alien = alienArray[i];
+    if (alien.alive) {
+      context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+    }
+  }
 }
 
 function moveShip(e) {
-  if (e.code == 'ArrowLeft' && ship.x - shipVelocityX >= 0) {
+  if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
     ship.x -= shipVelocityX; // move Left one tile
-  } else if (e.code == 'ArrowRight' && ship.x + shipVelocityX + ship.width <= board.width) {
+  } else if (
+    e.code == "ArrowRight" &&
+    ship.x + shipVelocityX + ship.width <= board.width
+  ) {
     ship.x += shipVelocityX; // move Right one tile
   }
 }
 
 function createAliens() {
-  
+  for (let c = 0; c < alienColumns; c++) {
+    for (let r = 0; r < alienRows; r++) {
+      let alien = {
+        img: alienImg,
+        x: alienX + c * alienWidth,
+        y: alienY + r * alienHeight,
+        width: alienWidth,
+        height: alienHeight,
+        alive: true,
+      };
+      alienArray.push(alien);
+    }
+  }
+  alienCount = alienArray.length;
 }
