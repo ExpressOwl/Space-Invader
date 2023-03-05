40760c247fc863f4770ec0985a0a10695bbb4ed4
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-03-05 16:15:19 +0000

    Canvas height and width defined
    Canvas defined. requestAnimationFrame is used instead of interval because its more accurate.
    ship is being moved through an update function which clears the canvas every keystroke.
    The moveShip function itself takes the event keycode and moves it left or right, but an operator is used to make sure it doesn't go out the canvas.

---
+++ space.js
@@ -0,0 +1,61 @@
+// board
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns;
+let boardHeight = tileSize * rows;
+let context;
+
+// ship
+let shipWidth = tileSize * 1.5;
+let shipHeight = tileSize;
+let shipX = tileSize * (columns / 2) - tileSize; // keeps the ship in the middle
+let shipY = tileSize * rows - tileSize * 2;
+
+let ship = {
+  x: shipX,
+  y: shipY,
+  width: shipWidth,
+  height: shipHeight,
+};
+
+let shipImg;
+let shipVelocityX = tileSize; //ship moving speed
+
+// Aliens
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.width = boardWidth;
+  board.height = boardHeight;
+  context = board.getContext("2d");
+
+  // Draw initial ship
+  shipImg = new Image();
+  shipImg.src = "/player.png";
+  shipImg.onload = function () {
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+  };
+
+  requestAnimationFrame(update);
+  document.addEventListener('keydown', moveShip);
+};
+
+function update() {
+  requestAnimationFrame(update)
+
+  context.clearRect(0, 0, board.width, board.height)
+
+  // ship redraw when moving
+  context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+}
+
+function moveShip(e) {
+  if (e.code == 'ArrowLeft' && ship.x - shipVelocityX >= 0) {
+    ship.x -= shipVelocityX; // move Left one tile
+  } else if (e.code == 'ArrowRight' && ship.x + shipVelocityX + ship.width <= board.width) {
+    ship.x += shipVelocityX; // move Right one tile
+  }
+}
